0ba8fd503758ce5cb698af7d3aea02aeeaa70b69
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-10-04 16:05:50 +0200

    Added floating navigation;

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,27 +4,29 @@ import Nav from './Nav';
 
 
 export default function HeroSection(): ReactElement {
-    return <section className={'heroSection'} id={'home'}>
+    return <>
         <Nav />
-        <div className={'details'}>
-            <h1>Your toolbox for streaming Dota 2</h1>
-            <div className={'description'}>
-                streamdota.com offers different tools to enrich your twitch stream with overlays, chat command and statistics.
-            </div>
+        <section className={'heroSection'} id={'home'}>
+            <div className={'details'}>
+                <h1>Your toolbox for streaming Dota 2</h1>
+                <div className={'description'}>
+                    streamdota.com offers different tools to enrich your twitch stream with overlays, chat command and statistics.
+                </div>
 
-            <div className={'learnMore'} onClick={() => animateScrollTo(document.getElementById('features'))}>
-                Learn more
+                <div className={'learnMore'} onClick={() => animateScrollTo(document.getElementById('features'))}>
+                    Learn more
+                </div>
             </div>
-        </div>
-        <div className="bottom">
-            <svg viewBox="0 0 1920 310" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className="svg replaced-svg">
-                <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                    <g transform="translate(0.000000, -554.000000)" fill="#FFFFFF">
-                        <path d="M-3,551 C186.257589,757.321118 319.044414,856.322454 395.360475,848.004007 C509.834566,835.526337 561.525143,796.329212 637.731734,765.961549 C713.938325,735.593886 816.980646,681.910577 1035.72208,733.065469 C1254.46351,784.220361 1511.54925,678.92359 1539.40808,662.398665 C1567.2669,645.87374 1660.9143,591.478574 1773.19378,597.641868 C1848.04677,601.75073 1901.75645,588.357675 1934.32284,557.462704 L1934.32284,863.183395 L-3,863.183395"></path>
+            <div className="bottom">
+                <svg viewBox="0 0 1920 310" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className="svg replaced-svg">
+                    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                        <g transform="translate(0.000000, -554.000000)" fill="#FFFFFF">
+                            <path d="M-3,551 C186.257589,757.321118 319.044414,856.322454 395.360475,848.004007 C509.834566,835.526337 561.525143,796.329212 637.731734,765.961549 C713.938325,735.593886 816.980646,681.910577 1035.72208,733.065469 C1254.46351,784.220361 1511.54925,678.92359 1539.40808,662.398665 C1567.2669,645.87374 1660.9143,591.478574 1773.19378,597.641868 C1848.04677,601.75073 1901.75645,588.357675 1934.32284,557.462704 L1934.32284,863.183395 L-3,863.183395"></path>
+                        </g>
                     </g>
-                </g>
-            </svg>
-        </div>
+                </svg>
+            </div>
+        </section>
         
         <style jsx>{`
             .heroSection {
@@ -95,5 +97,5 @@ export default function HeroSection(): ReactElement {
                 color: var(--secondary-accent);
             }
         `}</style>
-    </section>;
+    </>;
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,24 +1,55 @@
-import { ReactElement, useCallback } from "react";
+import { ReactElement, useCallback, useEffect, useRef, useState } from "react";
 import animateScrollTo from 'animated-scroll-to';
+import classNames from "classnames";
 
 export default function Nav(): ReactElement {
+    const ref = useRef();
+    const [sticky, setSticky] = useState(false);
+
+    const observer = process.browser ? new IntersectionObserver(([{intersectionRatio}]) => setSticky(intersectionRatio !== 1)) : null;
+    useEffect(() => {
+        ref.current && observer && observer.observe(ref.current);
+
+        return () => observer.disconnect();
+    }, [ref])
+
     const scrollTo = useCallback((id: string) => {
         animateScrollTo(document.getElementById(id));
     }, []);
 
-    return <nav>
-        <img src={'/images/logo.PNG'} alt={'Streamdota logo'} />
+    return <div className={'menuContainer'} ref={ref}>
+        <div className={classNames('innerContainer', {sticky})}>
+            <nav>
+                <img src={'/images/logo.PNG'} alt={'Streamdota logo'} />
 
-        <div className={'linkList'}>
-            <div className={'link'} onClick={() => scrollTo('home')}>Home</div>
-            <div className={'link'} onClick={() => scrollTo('features')}>Features</div>
-            <div className={'link'} onClick={() => scrollTo('events')}>Used by</div>
-            <div className={'link'} onClick={() => scrollTo('about')}>About</div>
-            <a className={'link'} href={'https://app.streamdota.com'}>Login</a>
+                <div className={'linkList'}>
+                    <div className={'link'} onClick={() => scrollTo('home')}>Home</div>
+                    <div className={'link'} onClick={() => scrollTo('features')}>Features</div>
+                    <div className={'link'} onClick={() => scrollTo('events')}>Used by</div>
+                    <div className={'link'} onClick={() => scrollTo('about')}>About</div>
+                    <a className={'link'} href={'https://app.streamdota.com'}>Login</a>
+                </div>
+            </nav>
         </div>
 
 
         <style jsx>{`
+            .innerContainer {
+                position: fixed;
+                top: 0px;
+                left: 0px;
+                right: 0;               
+                z-index: 5;
+                background-color: transparent;
+                transition: box-shadow 240ms ease-in-out, background-color 240ms ease-in-out, 
+            }
+
+            .sticky {
+                background-color: #FFF;
+                box-shadow: 2px 2px 3rem rgba(0,0,0,.8);
+                color: #FFF;
+            }
+
             nav {
                 max-width: 1200px;
                 margin: 0 auto;
@@ -36,16 +67,6 @@ export default function Nav(): ReactElement {
                 font-size: .8rem;
             }
 
-            @media screen and (max-width: 700px) { 
-                .linkList {
-                    display: none;
-                }
-
-                nav {
-                    justify-content: space-around;
-                }
-            }
-
             .link {
                 color: rgba(255, 255, 255, .7);
                 margin: 0 1rem;
@@ -58,6 +79,24 @@ export default function Nav(): ReactElement {
             .link:hover {
                 color: #FFF;
             }
+
+            .sticky .link {
+                color: #666;
+            }
+
+            .sticky .link:hover {
+                color: #333;
+            }
+
+            @media screen and (max-width: 700px) { 
+                .linkList {
+                    display: none;
+                }
+
+                nav {
+                    justify-content: space-around;
+                }
+            }
         `}</style>
-    </nav>
+    </div>;
 }
