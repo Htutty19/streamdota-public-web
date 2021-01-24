018a7a3ac244df8c0eacdfadb3a553736e446bed
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-01-24 16:23:10 +0100

    Removed gtag

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-
-import App from 'next/app'
-import Router from 'next/router'
-
-import * as gtag from '../util/gtag'
-
-Router.events.on('routeChangeComplete', url => gtag.pageview(url))
-
-export default App;
