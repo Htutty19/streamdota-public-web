222d813ab5e3f5ffd50850ddefc4f160810cfc60
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-01-27 14:10:16 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
                     __html: `
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date(), {storage: 'none'});
+                      gtag('js', new Date());
                       gtag('config', '${GA_TRACKING_ID}', {page_path: window.location.pathname});
                   `}}
                 />
