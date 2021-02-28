be8659197a422710a4c8ed5280158d28176352ce
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-02-28 09:52:21 +0100

    Adjusted texts, updated packages;

--- components/sections/Features.tsx
+++ components/sections/Features.tsx
@@ -1,179 +1,201 @@
-import { ReactElement } from "react";
+import AntiSnipeFeatures from "./Features/AntiSnipeFeatures";
+import CardRow from "./Features/CardRow";
 import Container from "../Container";
+import DotaWLFeatures from "./Features/DotaWLFeatures";
+import DraftStatsFeatures from "./Features/DraftStatsFeatures";
+import HeroStatsFeatures from "./Features/HeroStatsFeatures";
+import HowDoesStreamdotaWork from "./HowDoesStreamdotaWork";
+import Image from "next/image";
+import { ReactElement } from "react";
+import RoshanFeatures from "./Features/RoshanFeatures";
 import Section from "../Section";
 import SectionDescription from "../SectionDescription";
 import SectionHeader from "../SectionHeader";
 import TwoColumnContainer from "../TwoColumnContainer";
-import AntiSnipeFeatures from "./Features/AntiSnipeFeatures";
-import CardRow from "./Features/CardRow";
-import DotaWLFeatures from "./Features/DotaWLFeatures";
-import DraftStatsFeatures from "./Features/DraftStatsFeatures";
-import HeroStatsFeatures from "./Features/HeroStatsFeatures";
-import RoshanFeatures from "./Features/RoshanFeatures";
 import VoteSystemFeatures from "./Features/VoteSystemFeatures";
-import HowDoesStreamdotaWork from "./HowDoesStreamdotaWork";
-import Image from 'next/image';
-
 
 export default function Features(): ReactElement {
-    return <>
-        <Section id={'features'}>
-            <SectionHeader title={'What features does streamdota offer?'} />
-            <SectionDescription>A collection of overlays to enrich your stream with statistics and information</SectionDescription>
-            <CardRow />
-        </Section>
+  return (
+    <>
+      <Section id={"features"}>
+        <SectionHeader title={"What features does streamdota offer?"} />
+        <SectionDescription>
+          A collection of overlays to enrich your stream with statistics and
+          information
+        </SectionDescription>
+        <CardRow />
+      </Section>
 
-        <HowDoesStreamdotaWork />
+      <HowDoesStreamdotaWork />
 
-        <Section id={'voteSystem'} bg={'blueGrey'}>
-            <Container>
-                <div className={'featureContainer'}>
-                    <div className={'description'}>
-                        <h3>Vote System</h3>
-                        
-                        <TwoColumnContainer reverseWrap>
-                            <VoteSystemFeatures />
-                            <div className={'preview'}>
-                                <video width={'100%'} autoPlay loop playsInline muted>
-                                    <source src={'/videos/vote_system_preview.mp4'} type="video/mp4" />
-                                    Your browser does not support the video tag.
-                                </video>
-                            </div>
-                        </TwoColumnContainer>
-                    </div>
+      <Section id={"voteSystem"} bg={"blueGrey"}>
+        <Container>
+          <div className={"featureContainer"}>
+            <div className={"description"}>
+              <h3>Vote System</h3>
+
+              <TwoColumnContainer reverseWrap>
+                <VoteSystemFeatures />
+                <div className={"preview"}>
+                  <video width={"100%"} autoPlay loop playsInline muted>
+                    <source
+                      src={"/videos/vote_system_preview.mp4"}
+                      type="video/mp4"
+                    />
+                    Your browser does not support the video tag.
+                  </video>
                 </div>
-            </Container>
-        </Section>
+              </TwoColumnContainer>
+            </div>
+          </div>
+        </Container>
+      </Section>
 
-        <Section id={'dotaWL'}>
-            <Container>
-                <div className={'featureContainer'}>
-                    <div className={'description'}>
-                        <h3>Gameplay Tool - Dota Win/Loss Overlay</h3>
-                        
-                        <TwoColumnContainer>
-                            <div className={'preview imagePreview'}>
-                                <Image src={'/images/winLoss.jpg'} alt={'Win Loss Overlay'} width={424} height={191} />
-                            </div>
+      <Section id={"dotaWL"}>
+        <Container>
+          <div className={"featureContainer"}>
+            <div className={"description"}>
+              <h3>Gameplay Tool - Dota Win/Loss Overlay</h3>
 
-                            <DotaWLFeatures />
-                        </TwoColumnContainer>
-                    </div>
+              <TwoColumnContainer>
+                <div className={"preview imagePreview"}>
+                  <Image
+                    src={"/images/winLoss.jpg"}
+                    alt={"Win Loss Overlay"}
+                    width={424}
+                    height={191}
+                  />
                 </div>
-            </Container>
-        </Section>
 
-        <Section id={'antiSnipeMap'} bg={'blueGrey'}>
-            <Container>
-                <div className={'featureContainer'}>
-                    <div className={'description'}>
-                        <h3>Gameplay Tool - Anti Snipe Minimap <span className={'newLabel'}>NEW</span></h3>
-                        
-                        <TwoColumnContainer reverseWrap>
-                            <AntiSnipeFeatures />
+                <DotaWLFeatures />
+              </TwoColumnContainer>
+            </div>
+          </div>
+        </Container>
+      </Section>
 
-                            <div className={'preview imagePreview minimap'}>
-                                <Image src={'/images/snipeMap.png'} alt={'Win Loss Overlay'} width={320} height={180} />
-                            </div>
-                        </TwoColumnContainer>
-                    </div>
+      <Section id={"antiSnipeMap"} bg={"blueGrey"}>
+        <Container>
+          <div className={"featureContainer"}>
+            <div className={"description"}>
+              <h3>Gameplay Tool - Anti Snipe Minimap</h3>
+
+              <TwoColumnContainer reverseWrap>
+                <AntiSnipeFeatures />
+
+                <div className={"preview imagePreview minimap"}>
+                  <Image
+                    src={"/images/snipeMap.png"}
+                    alt={"Win Loss Overlay"}
+                    width={320}
+                    height={180}
+                  />
                 </div>
-            </Container>
-        </Section>
+              </TwoColumnContainer>
+            </div>
+          </div>
+        </Container>
+      </Section>
 
-        <Section id={'roshanTimer'}>
-            <Container>
-                <div className={'featureContainer'}>
-                    <div className={'description'}>
-                        <h3>Observer Tool - Roshan Timer</h3>
-                        
-                        <TwoColumnContainer>
-                            <div className={'preview imagePreview'}>
-                                <Image src={'/images/roshanTimer.jpg'} alt={'Roshan timer'} width={424} height={191} />
-                            </div>
-                            <RoshanFeatures />
-                        </TwoColumnContainer>
-                    </div>
+      <Section id={"roshanTimer"}>
+        <Container>
+          <div className={"featureContainer"}>
+            <div className={"description"}>
+              <h3>Observer Tool - Roshan Timer</h3>
+
+              <TwoColumnContainer>
+                <div className={"preview imagePreview"}>
+                  <Image
+                    src={"/images/roshanTimer.jpg"}
+                    alt={"Roshan timer"}
+                    width={424}
+                    height={191}
+                  />
                 </div>
-            </Container>
-        </Section>
+                <RoshanFeatures />
+              </TwoColumnContainer>
+            </div>
+          </div>
+        </Container>
+      </Section>
 
-        <Section id={'draftStats'} bg={'blueGrey'}>
-            <Container>
-                <div className={'featureContainer'}>
-                    <div className={'description'}>
-                        <h3>Observer Tool - Draft Stats <span className={'newLabel'}>NEW</span></h3>
-                        
-                        <TwoColumnContainer reverseWrap>
-                            <DraftStatsFeatures />
-                            <video width={'100%'} autoPlay loop playsInline muted>
-                                <source src={'/videos/draft_stats.mp4'} type="video/mp4" />
-                                Your browser does not support the video tag.
-                            </video>
-                        </TwoColumnContainer>
-                    </div>
-                </div>
-            </Container>
-        </Section>
+      <Section id={"draftStats"} bg={"blueGrey"}>
+        <Container>
+          <div className={"featureContainer"}>
+            <div className={"description"}>
+              <h3>Observer Tool - Draft Stats </h3>
 
-        <Section id={'heroStats'}>
-            <Container>
-                <div className={'featureContainer'}>
-                    <div className={'description'}>
-                        <h3>Observer Tool - Hero Stats <span className={'newLabel'}>NEW</span></h3>
-                        
-                        <TwoColumnContainer>
-                            <video width={'100%'} autoPlay loop playsInline muted>
-                                <source src={'/videos/hero_stats.mp4'} type="video/mp4" />
-                                Your browser does not support the video tag.
-                            </video>
-                            <HeroStatsFeatures />
-                        </TwoColumnContainer>
-                    </div>
-                </div>
-            </Container>
-        </Section>
+              <TwoColumnContainer reverseWrap>
+                <DraftStatsFeatures />
+                <video width={"100%"} autoPlay loop playsInline muted>
+                  <source src={"/videos/draft_stats.mp4"} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </TwoColumnContainer>
+            </div>
+          </div>
+        </Container>
+      </Section>
 
-        <style jsx>{`
-            .featureContainer {
-                padding: 0 1rem;
-            }
+      <Section id={"heroStats"}>
+        <Container>
+          <div className={"featureContainer"}>
+            <div className={"description"}>
+              <h3>Observer Tool - Hero Stats </h3>
 
-            .imagePreview {
-                display: flex;
-                justify-content: center;
-            }
+              <TwoColumnContainer>
+                <video width={"100%"} autoPlay loop playsInline muted>
+                  <source src={"/videos/hero_stats.mp4"} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+                <HeroStatsFeatures />
+              </TwoColumnContainer>
+            </div>
+          </div>
+        </Container>
+      </Section>
 
-            .imagePreview img {
-                transform: scale(1.3);
-                max-width: 80%;
-                object-fit: contain;
-                margin: 2rem 0;
-            }
+      <style jsx>{`
+        .featureContainer {
+          padding: 0 1rem;
+        }
 
-            .minimap {
-                max-width: 400px;
-                margin: 0 auto;
-                height:260px;
-            }
+        .imagePreview {
+          display: flex;
+          justify-content: center;
+        }
 
-            .newLabel {
-                background-color: var(--primary-accent);
-                padding: .25rem .5rem;
-                border-radius: .2rem;
-                color: #000;
-                text-transform: uppercase;
-                font-size: .8rem;
-                font-weight: bold;
-                margin-left: 1rem;
-                vertical-align: .1rem;
-            }
+        .imagePreview img {
+          transform: scale(1.3);
+          max-width: 80%;
+          object-fit: contain;
+          margin: 2rem 0;
+        }
 
-            @media screen and (max-width: 1100px) { 
-                h3 {
-                    text-align: center;
-                }
-            }
-        `}</style>
-    </>;
+        .minimap {
+          max-width: 400px;
+          margin: 0 auto;
+          height: 260px;
+        }
+
+        .newLabel {
+          background-color: var(--primary-accent);
+          padding: 0.25rem 0.5rem;
+          border-radius: 0.2rem;
+          color: #000;
+          text-transform: uppercase;
+          font-size: 0.8rem;
+          font-weight: bold;
+          margin-left: 1rem;
+          vertical-align: 0.1rem;
+        }
+
+        @media screen and (max-width: 1100px) {
+          h3 {
+            text-align: center;
+          }
+        }
+      `}</style>
+    </>
+  );
 }
--- components/sections/UsedByEvents.tsx
+++ components/sections/UsedByEvents.tsx
@@ -1,127 +1,146 @@
-import { ReactElement } from "react";
 import Card from "../Card";
 import Container from "../Container";
+import Image from "next/image";
+import { ReactElement } from "react";
 import Section from "../Section";
 import SectionHeader from "../SectionHeader";
 import TwoColumnContainer from "../TwoColumnContainer";
-import Image from 'next/image';
-
 
 export default function UsedByEvents(): ReactElement {
-    return <Section bg={'blueGrey'} id={'events'}>
-        <SectionHeader title={'Which events used streamdota?'} />
-        <Container>
-            <div className={'content'}>
-                <div className={'row'}>
-                    <Card>
-                        <div className={'cardContent'}>
-                            <TwoColumnContainer>
-                                <div className={'logo'}>
-                                    <Image src={'/images/events/eslm.png'} alt={'ESL Meisterschaft Logo'} height={200} width={193} />
-                                </div>
+  return (
+    <Section bg={"blueGrey"} id={"events"}>
+      <SectionHeader title={"Which events used streamdota?"} />
+      <Container>
+        <div className={"content"}>
+          <div className={"row"}>
+            <Card>
+              <div className={"cardContent"}>
+                <TwoColumnContainer>
+                  <div className={"logo"}>
+                    <Image
+                      src={"/images/events/eslm.png"}
+                      alt={"ESL Meisterschaft Logo"}
+                      height={200}
+                      width={193}
+                    />
+                  </div>
 
-                                <div className={'details'}>
-                                    <h3>ESL Meisterschaft 2020 Dota 2 - Season 2</h3>
-                                    <div className={'description'}>
-                                        We have created a custom branded vote system.<br /> Additionally we provide a roshan timer, draft stats and hero stats.
-                                    </div>
-                                </div>
-                            </TwoColumnContainer>
-                        </div>
-                    </Card>
-                </div>
+                  <div className={"details"}>
+                    <h3>ESL Meisterschaft 2020 Dota 2</h3>
+                    <div className={"description"}>
+                      We have created a custom branded vote system.
+                      <br /> Additionally we provide a roshan timer, draft stats
+                      and hero stats.
+                    </div>
+                  </div>
+                </TwoColumnContainer>
+              </div>
+            </Card>
+          </div>
 
-                <div className={'row'}>
-                    <Card>
-                        <div className={'cardContent'}>
-                            <TwoColumnContainer reverseWrap>
-                                <div className={'details'}>
-                                    <h3>Fragster DotA 2 Showdown</h3>
-                                    <div className={'description'}>
-                                        We provided a roshan timer and our vote system.
-                                    </div>
-                                </div>
+          <div className={"row"}>
+            <Card>
+              <div className={"cardContent"}>
+                <TwoColumnContainer reverseWrap>
+                  <div className={"details"}>
+                    <h3>Fragster DotA 2 Showdown</h3>
+                    <div className={"description"}>
+                      We provided a roshan timer and our vote system.
+                    </div>
+                  </div>
 
-                                <div className={'logo fragster'}>
-                                    <Image src={'/images/events/fragster.png'} alt={'Fragster Logo'} height={60} width={300} />
-                                </div>
-                            </TwoColumnContainer>
-                            </div>
-                    </Card>
-                </div>
+                  <div className={"logo fragster"}>
+                    <Image
+                      src={"/images/events/fragster.png"}
+                      alt={"Fragster Logo"}
+                      height={60}
+                      width={300}
+                    />
+                  </div>
+                </TwoColumnContainer>
+              </div>
+            </Card>
+          </div>
 
-                <div className={'row'}>
-                    <Card>
-                        <div className={'cardContent'}>
-                            <TwoColumnContainer>
-                                <div className={'logo'}>
-                                    <Image src={'/images/events/acl.png'} alt={'ACL'} height={210} width={200} />
-                                </div>
+          <div className={"row"}>
+            <Card>
+              <div className={"cardContent"}>
+                <TwoColumnContainer>
+                  <div className={"logo"}>
+                    <Image
+                      src={"/images/events/acl.png"}
+                      alt={"ACL"}
+                      height={210}
+                      width={200}
+                    />
+                  </div>
 
-                                <div className={'details'}>
-                                    <h3>ACL Masters #1 / #2 / #3</h3>
-                                    <div className={'description'}>
-                                        We provided our vote system. For ACL Masters #3 we provided our roshan timer and hero stats.
-                                    </div>
-                                </div>
-                            </TwoColumnContainer>
-                        </div>
-                    </Card>
-                </div>
-            </div>
-        </Container>
+                  <div className={"details"}>
+                    <h3>ACL Masters</h3>
+                    <div className={"description"}>
+                      We provided our vote system, roshan timer and further
+                      stats for ACL Masters.
+                    </div>
+                  </div>
+                </TwoColumnContainer>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </Container>
 
-        <style jsx>{`
-            .content {
-                padding: 0 5rem;
-            }
+      <style jsx>{`
+        .content {
+          padding: 0 5rem;
+        }
 
-            .cardContent {
-                padding: 3rem;
-            }
+        .cardContent {
+          padding: 3rem;
+        }
 
-            .logo {
-                max-width: 200px;
-                margin: 0 auto;
-            }
+        .logo {
+          max-width: 200px;
+          margin: 0 auto;
+        }
 
-            .row {
-                padding: 1rem 0;
-            }
+        .row {
+          padding: 1rem 0;
+        }
 
-            .description {
-                font-size: .9rem;
-                color: #444;
-            }
+        .description {
+          font-size: 0.9rem;
+          color: #444;
+        }
 
-            h3 {
-                margin-top: 0;
-            }
+        h3 {
+          margin-top: 0;
+        }
 
-            img {
-                object-fit: cover;
-                width: 100%;
-            }
+        img {
+          object-fit: cover;
+          width: 100%;
+        }
 
-            .fragster {
-                max-width: 300px;
-            }
+        .fragster {
+          max-width: 300px;
+        }
 
-            @media screen and (max-width: 1100px) { 
-                .content {
-                    text-align: center;
-                }
-            }
+        @media screen and (max-width: 1100px) {
+          .content {
+            text-align: center;
+          }
+        }
 
-            @media screen and (max-width: 700px) { 
-                .content {
-                    padding: 0 2rem;
-                }
+        @media screen and (max-width: 700px) {
+          .content {
+            padding: 0 2rem;
+          }
 
-                .cardContent {
-                    padding: 1rem;
-                }
-            }
-        `}</style>
-    </Section>;
+          .cardContent {
+            padding: 1rem;
+          }
+        }
+      `}</style>
+    </Section>
+  );
 }
